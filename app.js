01e30da58ad9451d4482bee5d95de4fd43a9eab6
shivam5445 <shivamchaurasiya5445@gmail.com>
2024-02-11 20:33:30 +0530

    add css and fix wrong city

--- app.js
+++ app.js
@@ -1,55 +1,51 @@
 const express = require("express");
 const https = require("https");
-
-const bodyparser = require("body-parser");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyparser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
 
-app.get("/" ,function(req,res){
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
     res.sendFile(__dirname +"/index.html");
-    
-    
-    // res.send("Server is up and running");
-})
+});
 
-app.post("/" , function(req,res){
-    console.log(req.body.cityname);
-    // console.log("post request recieved . ");
+app.post("/", function(req, res) {
     const query = req.body.cityname;
-    const api = "ed8c428d1828480c41516466bb56f604"
-    const unit ="metric"
-    const url="https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + api + "&units=" + unit;
-    https.get(url,function(response){
-        console.log(response.statusCode);
-        response.on("data",function(data){
-        // console.log(data);
-        const Weatherdata = JSON.parse(data);
-        // console.log(Weatherdata);
-        // const object = {
-        //     name  : "Shivam",
-        //     Favfood : "Paneer"
-        // }
-        // console.log(JSON.stringify(object));
-        const temp = Weatherdata.main.temp;
-        const weatherDiscription = Weatherdata.weather[0].description;
-        const icon = Weatherdata.weather[0].icon;
-        const imageurl = "https://openweathermap.org/img/wn/"+ icon + "@2x.png";
+    const api = "ed8c428d1828480c41516466bb56f604";
+    const unit = "metric";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${api}&units=${unit}`;
+    
+    https.get(url, function(response) {
+        if (response.statusCode === 404) {
+            // City not found
+            res.send('<script>alert("City not found. Please enter a valid city name."); window.location.href = "/";</script>');
+        } else{
+        response.on("data", function(data) {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
 
-        res.write("<p>the weather discription is " + weatherDiscription  + "</p>");
-        res.write("<h1>the temperature is " + query +" " + temp + "degree celcius</h1>");
-        res.write("<img src="+ imageurl + ">");
-        res.send();
-        })
-    })
-})
+            res.render("weather", {
+                city: query,
+                temperature: temp,
+                description: weatherDescription,
+                iconUrl: imageUrl
+            });
+        });
+    }
+    }).on("error", function(error) {
+        // Handle other errors
+        console.error("Error:", error);
+        res.send('<script>alert("An error occurred. Please try again later."); window.location.href = "/";</script>');
+    });
+});
 
-
-
-
-
-
-app.listen(process.env.PORT ||3000,function(){
-    console.log("server is running at port 3000");
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server is running at port 3000");
 });
